628890c2d2445965d1cc7803ce21193000eddce7
mconiaris <mconiaris@gmail.com>
2014-09-11 17:09:05 -0400

    Moved app.rb methods to newly made application_helper.rb file.

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -1,3 +1,52 @@
 module ApplicationHelper
 
+  ########################
+  # Methods
+  ########################
+
+  # Takes in Markdown text and returns HTML
+  def render_to_html(text)
+    markdown = Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML,
+        :fenced_code_blocks => true,
+        :hard_wrap => true
+        )
+    # binding.pry
+    @rendered_text = markdown.render(text)
+  end
+
+  # Create a list of all documents/articles
+  def generate_documents_array
+    @documents = []
+    $redis.keys("*article:*").each do |key|
+      document = get_document_from_redis(key)
+      @documents.push(document)
+    end
+    @documents.sort_by! { |hsh| hsh["id"]}
+    @documents.reverse!
+    # binding.pry
+  end
+
+  # Get article from redis and turn it in
+  # to a hash
+  def get_document_from_redis(key)
+    raw_data = $redis.get(key)
+    JSON.parse(raw_data)
+    # binding.pry
+  end
+
+  def add_document_to_redis(doc)
+    # binding.pry
+    $redis.set(doc.key, doc.to_json)
+  end
+
+  def find_article(params)
+    documents = generate_documents_array
+    documents.each do |doc|
+      if doc["title"] == "##{params[:id]}"
+        return doc
+      end
+    end
+  end
+
 end
